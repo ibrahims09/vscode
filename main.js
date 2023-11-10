d5343afb84d0c24346f4ab636021abdae8457ba7
ibrahimovic <144237765+ibrahims09@users.noreply.github.com>
2023-11-10 09:20:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var person = {
+  name: "John", // Ersätt "John" med det önskade namnet
+  age: 30,       // Ersätt 30 med den önskade åldern
+  country: "Sverige" // Ersätt "Sverige" med det önskade landet
+console.log(person )
+
+};
+
+
