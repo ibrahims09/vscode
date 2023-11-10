c005718cccb321851b6cb20106732e9228390255
Moa Sandberg <moisan1999@gmail.com>
2023-11-10 11:46:28 +0100

    solved the json problems

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-
-
  const mylist = document.getElementById("mylist");
+ const listContainer = document.getElementById('list-container')
+
  async function getlist(){
   const response = await fetch ("data.json");
   if (response.ok){
+    console.log('lyckat');
+    const json  = await response.json();
+   json.forEach(list => rendermyList(list));
+  }else{
+    console.log(response.status);
+  }
+} 
 
-    const list  = await response.json();
-  }}
+
+function rendermyList(list){
+  const listItem = document.createElement('li');
+  listItem.textContent = `${list.eventName || ''} - ${list.start || ''} - ${list.end || ''} - ${list.eventInfo || ''}`;
+
+listContainer.appendChild(listItem);
+}
+
+getlist();
