7eed86d624d8d0ea0b2fba29861fe1c7b5cabbff
ibrahimovic <144237765+ibrahims09@users.noreply.github.com>
2023-11-10 10:23:23 +0100

    start working on js file and trying to fetch the files

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-var person = {
-  name: "John", // Ersätt "John" med det önskade namnet
-  age: 30,       // Ersätt 30 med den önskade åldern
-  country: "Sverige" // Ersätt "Sverige" med det önskade landet
-console.log(person )
-
-};
 
 
