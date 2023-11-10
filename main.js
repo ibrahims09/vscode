625bd84ad699aad542ff9897fae6ab8ebe85719b
ibrahimovic <144237765+ibrahims09@users.noreply.github.com>
2023-11-10 10:26:55 +0100

    start working on js file and trying to fetch the files

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open")
+  icon.classList.toggle("open")
+  
+ }
 
+ const mylist = docement.getelementBYid("mylist");
+ async function getlist(){
+  const response = await fetch ("data.json");
+  if (response.ok){
+
+    const list =await response.json();
+    console.log(list)
+    list.foreach(function(list){
+      const listelement = document.createElement("i");
+      listelement.innerhtml = listitem.titel;
+      mylist.appendchild(listelement);
+      const listBtn = document.createElement('button');
+      listBtn.textContent = "cross off";
+      listelement.appendchild(listBtn);
+      
+    }
+  }
+ }
 
